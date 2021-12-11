62d43b393fd29c753fb7e1a1df43ec6a46acb99a
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2021-12-11 18:22:02 +0300

    add class code

---
+++ Lesson2/webpack-example/src/js/main.js
@@ -0,0 +1,9 @@
+import "./pages/dateCalc.js";
+import css from '../css/style.css';
+
+import pictureUrl from '../image.jpg';
+
+const image = document.createElement('img');
+image.src = pictureUrl;
+
+document.body.appendChild(image);
---
+++ Lesson2/webpack-example/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve('src', 'js', 'main.js'),
+    output: {
+        filename: 'main.[contenthash].js',
+        clean: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name]].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve('src', 'template.html'),
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(jpg)$/i,
+                use: ('file-loader')
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
+        ]
+    }
+
+}
