48f87089f75f2fd875faee36ed4e4970a1c59a08
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2021-12-15 14:35:59 +0300

    add image-minimizer-webpack-plugin

--- Lesson3/src/js/main.js
+++ Lesson3/src/js/main.js
@@ -1,1 +1,72 @@
 import scss from "../css/style.scss";
+import img1 from "../media/1.jpg";
+import img2 from "../media/2.jpg";
+import img3 from "../media/3.jpg";
+import audio1 from '../media/1.mp3';
+import audio2 from '../media/1.mp3';
+import video from '../media/1.mp4';
+
+const media = [
+    {
+        src: img1,
+        title: 'Изображение 1',
+        type: 'image'
+    },
+    {
+        src: img2,
+        title: 'Изображение 2',
+        type: 'image'
+    },
+    {
+        src: img3,
+        title: 'Изображение 3',
+        type: 'image'
+    },
+    {
+        src: audio1,
+        title: 'Аудио 1',
+        type: 'sound'
+    },
+    {
+        src: audio2,
+        title: 'Аудио 2',
+        type: 'sound'
+    },
+    {
+        src: video,
+        title: 'Видео',
+        type: 'video'
+    }
+]
+
+const block = document.getElementsByClassName('gallery')[0];
+media.forEach(el => {
+    const div = document.createElement('div');
+    let element;
+    if (el.type == "image") {
+        element = document.createElement('img');
+        element.src = el.src;
+        div.appendChild(element);
+    } else if (el.type == "sound") {
+        element = document.createElement('audio');
+        element.setAttribute('controls', 'controls');
+        let sourse = document.createElement('source');
+        sourse.src = el.src;
+        sourse.type = "audio/mpeg";
+        element.appendChild(sourse);
+        div.appendChild(element);
+    } else if (el.type == "video") {
+        element = document.createElement('video');
+        element.setAttribute('controls', 'controls');
+        let sourse = document.createElement('source');
+        sourse.src = el.src;
+        sourse.type = "video/mp4";
+        element.appendChild(sourse);
+        div.appendChild(element);
+    }
+    const text = document.createElement('p');
+    text.textContent = el.title;
+    div.appendChild(text);
+    div.className = 'media';
+    block.appendChild(div);
+})
--- Lesson3/webpack.config.js
+++ Lesson3/webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== "production";
-console.log("devMod=" + devMode);
-
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { extendDefaultPlugins } = require("svgo");
 
+
+console.log("devMod=" + devMode + ' ' + process.env.NODE_ENV);
 module.exports = {
     entry: path.resolve('src', 'js', 'main.js'),
     output: {
         filename: 'main.[contenthash].js',
         clean: true
     },
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
@@ -18,9 +21,21 @@ module.exports = {
                 use: [
                     devMode ? "style-loader" : MiniCssExtractPlugin.loader,
                     "css-loader",
-                    "sass-loader",
-                ],
+                    "sass-loader"
+                ]
             },
+            {
+                test: /\.(mp3|mp4)$/i,
+                use: ('file-loader')
+
+            },
+            {
+                test: /\.(jpg)$/i,
+                type: "asset",
+                use: ('file-loader')
+
+            }
+
         ]
     },
     plugins: [
@@ -32,5 +47,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve('src', 'template.html'),
         })
-    ]
+    ],
+    optimization: {
+        minimizer: [
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.squooshMinify,
+                }
+            }),
+        ],
+    }
 }
